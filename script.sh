3965d2f87aab03fe7535d5216e2c7de571273185
cesc <saroj.santra@gmail.com>
2020-05-05 06:32:36 +0000

    first commit

---
+++ script.sh
@@ -0,0 +1,40 @@
+1  lshw
+    2  git
+    3  sudo apt-get install git
+    4  sudo apt-get install curl xzcat cpio
+    5  git clone https://github.com/patjak/facetimehd-firmware.git
+    6  sudo apt-get install git
+    7  git
+    8  sudo apt install git
+    9  apt-get update
+   10  sudo apt-get update
+   11  sudo apt-get install git
+   12  sudo apt-get install curl xzcat cpio
+   13  sudo apt-get install curl  cpio
+   14  git clone https://github.com/patjak/facetimehd-firmware.git
+   15  cd facetimehd-firmware
+   16  make
+   17  sudo apt install make      
+   18  sudo make install
+   19  make
+   20  sudo make install
+   21  cd ..
+   22  sudo apt-get install kmod libssl-dev checkinstall
+   23  sudo apt-get install kmod libssl-dev
+   24  git clone https://github.com/patjak/bcwc_pcie.git
+   25  cd bcwc_pcie/
+   26  make
+   27  gcc
+   28  sudo apt install gcc
+   29  sudo make install
+   30  sudo depmod
+   31  sudo modprobe -r bdc_pci
+   32  sudo modprobe facetimehd
+   33  make
+   34  sudo make install
+   35  sudo depmod
+   36  sudo modprobe -r bdc_pci
+   37  sudo modprobe facetimehd
+   38  sudo nano /etc/modules
+   39  cat /etc/modules
+   40  history
